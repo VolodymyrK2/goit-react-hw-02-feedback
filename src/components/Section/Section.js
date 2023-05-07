29c1ae0b7eef92c8f85d6a45a73cb91c0ce121f5
VolodymyrK2 <tema.rivne@gmail.com>
2023-05-07 13:31:18 +0300

    del unused

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,6 +1,5 @@
 import PropTypes from "prop-types";
 import css from "./Section.module.css";
-import FeedbackOptions from "components/FeedbackOptions/FeedbackOptions";
 const Section = ({title,children}) => {
     return (
         <section className={css.section}>
